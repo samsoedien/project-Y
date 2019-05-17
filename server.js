40cf5ae0e3222a5747581449ec4241ab9dfed52b
Samsoedien <n.samsoedien@gmail.com>
2019-05-17 19:48:43 +0200

    connect to mongodb

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+const app = require('./app');
+// const URI = require('./config/keys').MONGODB_URI;
+
+const port = process.env.PORT || 4000;
+const server = http.createServer(app);
+
+mongoose
+  .connect(process.env.MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB connected');
+    server.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch(err => {
+    throw new Error(err);
+  });
+mongoose.Promise = global.Promise;
