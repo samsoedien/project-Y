f46d13dee2b80cff0f87c6b3238152a49b44e9b3
Samsoedien <samsoedien@Samsoediens-MacBook-Pro.local>
2019-06-15 00:03:51 +0200

    add redux logger

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import rootReducer from '../reducers';
 
 const initialState = {};
 
-const middleware = [thunk];
+const middleware = [thunk, logger];
+
+// const composeEnhancers = composeWithDevTools();
 
 const store = createStore(
   rootReducer,
