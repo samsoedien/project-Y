f78a2882035a99b9c29176c817784191d50397b4
Samsoedien <samsoedien@Samsoediens-MacBook-Pro.local>
2019-06-14 17:20:46 +0200

    cleanup app.jsx and routes

--- server/config/keys/keys_dev.js
+++ server/config/keys/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  StripePublishableKey: '',
-  StripeSecretKey: '',
+  StripePublishableKey: 'pk_test_4yCPWy17iySrchC0vHFydfmP00Z572Olvq',
+  StripeSecretKey: 'sk_test_xaV51W9eydptpOzrt3ftup9a00LEKVOZ9z',
 };
