2e30fae9b822b8c70cc599be44a92777daef94f3
Samsoedien <samsoedien@Samsoediens-MacBook-Pro.local>
2019-06-26 23:33:39 +0200

    add password confirm validation

--- server/api/validation/users.js
+++ server/api/validation/users.js
@@ -4,9 +4,15 @@ module.exports = [
   check('name', 'Name is required')
     .not()
     .isEmpty(),
-  check('email', 'Please include email').isEmail(),
-  check('password', 'Please enter password with 6 or more characters').isLength(
+  check('email', 'Please include email')
+    .isEmail()
+    .withMessage('Please enter a valid email'),
+  body('password', 'Please enter password with 6 or more characters').isLength(
     { min: 6 },
+  body('confirmPassword').custom((value, { req }) => {
+    if (value !== req.body.password) throw new Error('Password do not match');
+    return true;
+  }),
   ),
 ];
 
